4ac7be4d6131872bbd7d62d53ccaa7d43d2ac624
Tony <astravius@outlook.com>
2024-02-02 13:33:53 -0800

    test q1

--- src/main/resources/static/js/quizLogic.js
+++ src/main/resources/static/js/quizLogic.js
@@ -18,34 +18,37 @@
 //     console.log(ans1);
 // })
 
-var answers = [0, 1, 2, 3, 4, 5]
+var answers = [];
+var key = [2, 3, 1, 1];
+var currQ = 1;
 
 let fn1 = function(evt){
     evt.preventDefault()
-    var ans1 = document.querySelector("#q1").value
-    // console.log(ans1)
+    var ans1 = document.querySelector('input[name="q1"]:checked').value; 
+    console.log(ans1)
 
     if(ans1 == 0){
         document.querySelector("#result").innerHTML = "select an answer"
         console.log("no input")
         document.getElementById("result").style.display="inline"
     }else{
-        if(ans1 == 1){
-            document.querySelector("#result").innerHTML = "incorrect"
-            console.log("incorrect")
-        }else if(ans1 == 2){
-            document.querySelector("#result").innerHTML = "correct"
+        if(ans1 == 2){
             console.log("correct")
-        }else if(ans1 == 3){
-            document.querySelector("#result").innerHTML = "incorrect"
+            document.querySelector("#result").innerHTML = "correct"
+            document.getElementById("result").style.color = "green"
+        }else{
             console.log("incorrect")
+            document.querySelector("#result").innerHTML = "incorrect"
+            document.getElementById("result").style.color = "red"
         }
-        // document.querySelector("#result").computedStyleMap.display = "block"
-        // document.querySelector("#result").style.display="block"
         document.getElementById("result").style.display="inline"
     }
-
+    answers.push(ans1);
     // document.querySelector("#result").innerHTML = ans1
+}
+
+let checkAns = function(evt){
+    
 }
 
 document.querySelector("#sub").addEventListener("click", fn1)
